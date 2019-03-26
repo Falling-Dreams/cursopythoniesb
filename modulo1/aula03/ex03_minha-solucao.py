c7d0ce2223df303d74218e13d87816e297bade72
Kevin Santana <kevin_santana.araujo@hotmail.com>
2019-03-26 16:48:46 -0300

    Adiciona solucao do ex03 da aula03

---
+++ modulo1/aula03/ex03_minha-solucao.py
@@ -0,0 +1,22 @@
+#3. Dada uma lista, avalie se algum dos numeros e multiplo de 7.
+lista = ["Joao", 18, "Python", "Maria", 20, "C", -10, 2, 8, 5, 7]
+impares = []
+n = 0
+s = ''
+n = ''
+t = ''
+#Retorna uma lista n com os numeros de uma lista mista
+for element in lista:
+	n = [x for x in lista if (isinstance(x, int))]
+#Itera sobre a lista retornada, verificando se ha multiplo de 7
+for a in n:
+	if (a % 7 == 0):
+		s = "Sim"
+		
+	elif (a % 7 != 0) :
+		n = "Nao"
+
+	elif (a % 7 == 0 and a % 7 != 0):
+		t = s + "/" +  n
+
+print("Existem multiplos de 7: " + s + " " + n + " " + t)
