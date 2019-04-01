d4c9c4f1f1fb50a223d1b36db68ac182624d1c97
Rodrigo Guimarães Araújo <rodrigoara27@gmail.com>
2019-04-01 20:19:18 -0300

    Avaliação

--- modulo1/aula02/ex02_minha-solucao.py
+++ modulo1/aula02/ex02_minha-solucao.py
@@ -1,3 +1,10 @@
+"""
+Avaliado por: Rodrigo Guimarãeas
+Nota: 10
+Observações: 
+    -
+"""
+
 #2. Dado dois numeros inteiros, crie um programa que retorne a divisao 
 #inteira entre eles e o resto de sua divisao.
 a = int(input("Valor a: "))
