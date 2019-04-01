7b44035d9ca590ce6e7eb6799b06bb72133a7fed
Rodrigo Guimarães Araújo <rodrigoara27@gmail.com>
2019-04-01 20:18:17 -0300

    Avaliação

--- modulo1/aula02/ex04._minha-solucao.py
+++ modulo1/aula02/ex04._minha-solucao.py
@@ -1,10 +1,18 @@
+"""
+Avaliado por: Rodrigo Guimarãeas
+Nota: 10
+Observações: 
+    - Parabéns pelo ord!
+    - Não precisa de parênteses no IF
+"""
+
 #4. Desenvolva um codigo onde e possivel verificar se uma letra informada e
 #maiuscula ou minuscula.
 
 #letra = int(input("Informe a letra: "))
 letra = ord(input("Informe a letra: "))
 
-if (letra >= 65 and letra <= 90):
+if (letra >= 65 and letra <= 90): # if letra >= 65 and letra <= 90
 	print("maiuscula")
 
 else:
