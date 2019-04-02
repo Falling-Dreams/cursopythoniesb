7867f357cfc7b06278a8f96d4a5a40777d11431b
Kevin Santana <kevin_santana.araujo@hotmail.com>
2019-04-01 22:29:13 -0300

    Corrige exercicio 4 da aula 03

--- modulo1/aula03/ex04_minha-solucao.py
+++ modulo1/aula03/ex04_minha-solucao.py
@@ -1,19 +1,3 @@
 #4. Ao informar um numero, responda se o mesmo e primo.
-#n = [x for x in range(2, 20) if all(x % y != 0 for y in range(2, x))]
-lista = ["Joao", 18, "Python", "Maria", 20, "C", -10, 2, 8, 5, 7]
-x = 0
-lista_primos = list()
-#Retorna lista de numeros e a soma total dos seus elementos,
-#a partir de uma lista mista
-for a in lista:
-	n = [x for x in lista if (isinstance(x, int))]
-	x = sum(n)
-
-for b in n:
-	for c in range(1, x):		
-		if(all(abs(b) % i for i in range(2, b))):
-			lista_primos.append(b)
-			break
-print(lista_primos)
-
-				
+x = 5
+print(all(x % i for i in xrange(2, x)))
---
+++ modulo1/aula05/ex02_minha-solucao.py
@@ -0,0 +1,18 @@
+#2. Cria uma função que ao receber uma lista retorne apenas 
+#os números primos da lista.
+
+lista = ["Joao", 18, "Python", "Maria", 20, "C", -10, 2, 8, 5, 7]
+x = 0
+lista_primos = list()
+#Retorna lista de numeros e a soma total dos seus elementos,
+#a partir de uma lista mista
+for a in lista:
+	n = [x for x in lista if (isinstance(x, int))]
+	x = sum(n)
+
+for b in n:
+	for c in range(1, x):		
+		if(all(abs(b) % i for i in range(2, b))):
+			lista_primos.append(b)
+			break
+print(lista_primos)
